279d4494ad0ddbecdf31402c92440217a37bf703
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-05-24 11:02:05 -0500

    .

--- SyncSaberService/Downloaders/BeastSaverDownloader.cs
+++ SyncSaberService/Downloaders/BeastSaverDownloader.cs
@@ -42,9 +42,9 @@ namespace SyncSaberService.Downloaders
         {
             get
             {
-                if(_feeds == null)
+                if (_feeds == null)
                 {
-                    _feeds = new Dictionary<int, string> ()
+                    _feeds = new Dictionary<int, string>()
                     {
                         { 0, "https://bsaber.com/members/" + USERNAMEKEY + "/wall/followings/feed/?acpage=" + PAGENUMKEY },
                         { 1, "https://bsaber.com/members/" + USERNAMEKEY + "/bookmarks/feed/?acpage=" + PAGENUMKEY },
@@ -72,12 +72,13 @@ return _cookieHeader;
             _username = username;
             _password = password;
             _loginUri = loginUri;
+            
         }
 
         public static CookieContainer GetBSaberCookies(string username, string password)
         {
             CookieContainer tempContainer = null;
-            lock(_cookies)
+            lock (_cookies)
             {
                 if (_cookies != null)
                 {
@@ -90,7 +91,7 @@ return _cookieHeader;
             string loginUri = "https://bsaber.com/wp-login.php?jetpack-sso-show-default-form=1";
             string reqString = $"log={username}&pwd={password}&rememberme=forever";
             var tempCookies = GetCookies(loginUri, reqString);
-            lock(_cookies)
+            lock (_cookies)
             {
                 _cookies = tempCookies;
             }
@@ -111,14 +112,63 @@ return _cookieHeader;
             using (Stream s = request.GetRequestStream())
                 s.Write(requestData, 0, requestData.Length);
             HttpWebResponse response = (HttpWebResponse) request.GetResponse(); // Needs this to populate cookies
+
             return cc;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pageText"></param>
+        /// <exception cref="XmlException">Invalid XML in pageText</exception>
+        /// <returns></returns>
         public SongInfo[] GetSongsFromPage(string pageText)
         {
-            throw new NotImplementedException();
+            List<SongInfo> songsOnPage = new List<SongInfo>();
+
+            int totalSongsForPage = 0;
+            XmlDocument xmlDocument = new XmlDocument();
+
+            xmlDocument.LoadXml(pageText);
+
+            XmlNodeList xmlNodeList = xmlDocument.DocumentElement.SelectNodes("/rss/channel/item");
+            foreach (object obj in xmlNodeList)
+            {
+                XmlNode node = (XmlNode) obj;
+                if (node["DownloadURL"] == null || node["SongTitle"] == null)
+                {
+                    Logger.Debug("Not a song! Skipping!");
+                }
+                else
+                {
+                    string songName = node["SongTitle"].InnerText;
+                    string innerText = node["DownloadURL"].InnerText;
+                    if (innerText.Contains("dl.php"))
+                    {
+                        Logger.Warning("Skipping BeastSaber download with old url format!");
+                        totalSongsForPage++;
+                    }
+                    else
+                    {
+                        string songIndex = innerText.Substring(innerText.LastIndexOf('/') + 1);
+                        string mapper = GetMapperFromBsaber(node.InnerText);
+                        string songUrl = "https://beatsaver.com/download/" + songIndex;
+                        SongInfo currentSong = new SongInfo(songIndex, songName, songUrl, mapper);
+                        string currentSongDirectory = Path.Combine(Config.BeatSaberPath, "CustomSongs", songIndex);
+                        //bool downloadFailed = false;
+                        songsOnPage.Add(currentSong);
+                    }
+                }
+            }
+            return songsOnPage.ToArray();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <returns></returns>
         public string GetPageText(string url)
         {
             HttpClient hClient = new HttpClient();
@@ -156,6 +206,23 @@ return _cookieHeader;
         public string GetPageUrl(int feedIndex, int page)
         {
             return GetPageUrl(FeedUrls[feedIndex], page);
+        }
+
+        private static string GetMapperFromBsaber(string innerText)
+        {
+            //TODO: Needs testing for when a mapper's name isn't obvious
+            string prefix = "Mapper: ";
+            string suffix = "</p>";
+
+            int startIndex = innerText.IndexOf(prefix);
+            if (startIndex < 0)
+                return "";
+            startIndex += prefix.Length;
+            int endIndex = innerText.IndexOf(suffix, startIndex);
+            if (endIndex > startIndex && startIndex >= 0)
+                return innerText.Substring(startIndex, endIndex - startIndex);
+            else
+                return "";
         }
     }
 }
